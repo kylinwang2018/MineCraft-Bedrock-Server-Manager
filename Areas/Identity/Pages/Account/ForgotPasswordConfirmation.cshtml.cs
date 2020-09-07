3b0a2583cffa29afd7f2e789272320669fcc5539
Ben Wang <wyqmlm66@gmail.com>
2020-09-07 11:08:45 +1000

    Add Identity to project

---
+++ Areas/Identity/Pages/Account/ForgotPasswordConfirmation.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MineCraft_Bedrock_Server_Manager.Areas.Identity.Pages.Account
+{
+    [AllowAnonymous]
+    public class ForgotPasswordConfirmation : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
