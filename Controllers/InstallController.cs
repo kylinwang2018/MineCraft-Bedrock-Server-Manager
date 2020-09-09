ca3b46aa0262bf28638e169bf0276f16fe634b3e
kylinwang <wyqmlm66@gmail.com>
2020-09-09 19:17:00 +1000

    Add business logic at Install page

--- Controllers/InstallController.cs
+++ Controllers/InstallController.cs
@@ -5,30 +5,97 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using MineCraft_Bedrock_Server_Manager.Models;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
+using MineCraft_Bedrock_Server_Manager.Areas.Identity.Pages.Account;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.WebUtilities;
+using System.Text;
+using System.Text.Encodings.Web;
 
 namespace MineCraft_Bedrock_Server_Manager.Controllers
 {
     public class InstallController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly SignInManager<IdentityUser> _signInManager;
         private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly ILogger<RegisterModel> _logger;
+        private readonly IEmailSender _emailSender;
 
         public InstallController(
-            ILogger<HomeController> logger, 
-            UserManager<IdentityUser> userManager
+            UserManager<IdentityUser> userManager,
+            SignInManager<IdentityUser> signInManager,
+            RoleManager<IdentityRole> roleManager,
+            ILogger<RegisterModel> logger,
+            IEmailSender emailSender
             )
         {
-            _logger = logger;
             _userManager = userManager;
+            _signInManager = signInManager;
+            _logger = logger;
+            _emailSender = emailSender;
+            _roleManager = roleManager;
         }
 
+        public string ReturnUrl { get; set; }
+
+        public IList<AuthenticationScheme> ExternalLogins { get; set; }
+
+        [HttpGet]
         public IActionResult Index()
+        { 
+            if (_userManager.Users.Count() > 0)
+              return RedirectToAction("Index","Home");
+            return View(new InstallViewModel());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(InstallViewModel model)
         {
             if (_userManager.Users.Count() > 0)
-                return RedirectToAction("Index","Home");
-            return View();
+              return RedirectToAction("Index","Home");
+
+            var returnUrl = Url.Content("~/");
+            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser { UserName = model.Input.Email, Email = model.Input.Email };
+                var result = await _userManager.CreateAsync(user, model.Input.Password);
+                if (result.Succeeded)
+                {
+                    await _userManager.AddToRoleAsync(user, "ADMIN");
+                    _logger.LogInformation("User created a new account with password.");
+
+                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+                    var callbackUrl = Url.Page(
+                        "/Account/ConfirmEmail",
+                        pageHandler: null,
+                        values: new { area = "Identity", userId = user.Id, code = code, returnUrl = returnUrl },
+                        protocol: Request.Scheme);
+
+                    await _emailSender.SendEmailAsync(model.Input.Email, "Confirm your email",
+                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+
+                    if (_userManager.Options.SignIn.RequireConfirmedAccount)
+                    {
+                        return RedirectToPage("RegisterConfirmation", new { email = model.Input.Email, returnUrl = returnUrl });
+                    }
+                    else
+                    {
+                        await _signInManager.SignInAsync(user, isPersistent: false);
+                        return LocalRedirect(returnUrl);
+                    }
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+
+            return View(new InstallViewModel());
         }
     }
 }
---
+++ Models/InstallViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+namespace MineCraft_Bedrock_Server_Manager.Models
+{
+    public class InstallViewModel
+    {
+
+        public InstallViewModel()
+        {
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public class InputModel
+        {
+            [Required]
+            [EmailAddress]
+            [Display(Name = "Email")]
+            public string Email { get; set; }
+
+            [Required]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [DataType(DataType.Password)]
+            [Display(Name = "Password")]
+            public string Password { get; set; }
+
+            [DataType(DataType.Password)]
+            [Display(Name = "Confirm password")]
+            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            public string ConfirmPassword { get; set; }
+        }
+    }
+}
