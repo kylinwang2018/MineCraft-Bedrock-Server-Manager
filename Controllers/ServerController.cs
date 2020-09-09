70948662b13fbf7a83c353352ec199f22c8fd15a
kylinwang <wyqmlm66@gmail.com>
2020-09-09 20:11:52 +1000

    add Server Controller

---
+++ Controllers/ServerController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MineCraft_Bedrock_Server_Manager.Controllers
+{
+    public class ServerController : Controller
+    {
+        public ServerController()
+        {
+        }
+
+        public IActionResult Status()
+        {
+            return View();
+        }
+
+        public IActionResult Update()
+        {
+            return View();
+        }
+
+        public IActionResult Configuration()
+        {
+            return View();
+        }
+
+        public IActionResult Permissions()
+        {
+            return View();
+        }
+
+        public IActionResult Backup()
+        {
+            return View();
+        }
+    }
+}
